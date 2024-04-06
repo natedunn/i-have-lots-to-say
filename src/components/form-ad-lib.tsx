c19543e36d881e6c437ee7226a66082e11e02c6b
Nate Dunn <hello@natedunn.net>
2024-04-05 18:59:33 -0600

    fix: state bug and design adjustments

--- src/components/form-ad-lib.tsx
+++ src/components/form-ad-lib.tsx
@@ -1,14 +1,21 @@
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
-import { Separator } from "./ui/separator";
+import { useEffect, useState, type PropsWithChildren } from "react";
 import { Checkbox } from "./ui/checkbox";
 
 const DEFAULTS = {
 	newTo: "Github",
 	noFucksFor: "CODE",
 	justWantTo: "download this stupid fucking application",
+};
+
+const Text = ({ children }: PropsWithChildren) => {
+	return (
+		<span className="!text-white underline decoration-zinc-400 underline-offset-2 decoration-2">
+			{children}
+		</span>
+	);
 };
 
 export function FormAdLib() {
@@ -37,9 +44,17 @@ export function FormAdLib() {
 		}
 	}, [textToCopy]);
 
-	const getNewTo = () => newTo ?? DEFAULTS.newTo;
-	const getNoFucksFor = () => noFucksFor ?? DEFAULTS.noFucksFor;
-	const getJustWantTo = () => justWantTo ?? DEFAULTS.justWantTo;
+	useEffect(() => {
+		if (newTo === "") {
+			setNewTo(DEFAULTS.newTo);
+		}
+		if (justWantTo === "") {
+			setJustWantTo(DEFAULTS.justWantTo);
+		}
+		if (noFucksFor === "") {
+			setNoFucksFor(DEFAULTS.noFucksFor);
+		}
+	}, [newTo, justWantTo, noFucksFor]);
 
 	return (
 		<div className="w-full">
@@ -89,23 +104,28 @@ export function FormAdLib() {
 				</div>
 			</div>
 			<div
-				className="mt-8 border-t grid gap-4 p-10 font-mono bg-muted text-lg"
+				className="mt-8 border-t grid gap-4 p-10 font-mono bg-muted text-zinc-400 text-lg"
 				id="text-to-copy"
 			>
 				<p>
-					{disableNewTo !== true && `I am new to ${getNewTo()} and `}I have lots
-					to say
+					{disableNewTo !== true && (
+						<span>
+							I am new to <Text>{newTo}</Text> and{" "}
+						</span>
+					)}
+					I have lots to say
 				</p>
 				<p>
-					I DONT GIVE A FUCK ABOUT THE FUCKING {getNoFucksFor()}! i just want to{" "}
-					{getJustWantTo()}
+					I DONT GIVE A FUCK ABOUT THE FUCKING <Text>{noFucksFor}</Text>! i just
+					want to <Text>{justWantTo}</Text>.{` `}
 				</p>
 				<p>
-					WHY IS THERE {getNoFucksFor()}??? MAKE A FUCKING SOLUTION AND GIVE IT
-					TO ME. these dumbfucks think that everyone is a developer and
-					understands {getNoFucksFor()}. well i am not and i don't understand
-					it. I only know to download and install applications. SO WHY THE FUCK
-					IS THERE {getNoFucksFor()}? STUPID FUCKING SMELLY NERDS
+					WHY IS THERE <Text>{noFucksFor}</Text>??? MAKE A FUCKING SOLUTION AND
+					GIVE IT TO ME. these dumbfucks think that everyone is a developer and
+					understands <Text>{noFucksFor}</Text>. well i am not and i don't
+					understand it. I only know to download and install applications. SO
+					WHY THE FUCK IS THERE <Text>{noFucksFor}</Text>? STUPID FUCKING SMELLY
+					NERDS
 				</p>
 			</div>
 			<div className="mt-6 flex items-center gap-3">
